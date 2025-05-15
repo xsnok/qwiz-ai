8031905e860a8b446e5f5d405ff9ca38f5b25381
Snok <chocowaco152@gmail.noreply.github.com>
2025-05-14 20:27:59 -0700

    Set up basic files Created test files Made public folder for css

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+const express = require("express")
+const app = express()
+const fs = require('fs')
+
+app.set('view engine', 'ejs')
+app.listen(3000)
+
+app.use(express.static('public'))
+app.use((req, res, next) => {
+    console.log('this is middle-ware')
+    next()
+})
+
+app.get('/', (req,res) => {
+    res.setHeader('Content-Type', 'text/html')
+    const title = "blud"
+    res.render('index', {title})
+})
