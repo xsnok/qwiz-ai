c187a8a847de49873ece491bd130395cbbea56a2
Snok <chocowaco152@gmail.noreply.github.com>
2025-05-17 13:39:21 -0700

    Add mongoDB and separate css to each page Add a create blog webpage Add blog mongoDB schema

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express")
+const creds = require('./creds')
+const Blog = require('./models/blog')
+const mongoose = require('mongoose')
 const app = express()
-const fs = require('fs')
 
 app.set('view engine', 'ejs')
-app.listen(3000)
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+mongoose.connect(creds.mongoAPI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
 app.use((req, res, next) => {
     console.log('this is middle-ware')
     next()
@@ -15,4 +22,43 @@ app.get('/', (req,res) => {
     res.setHeader('Content-Type', 'text/html')
     const title = "blud"
     res.render('index', {title})
+})
+
+app.get('/blog/add', (req, res) => {
+    const blog = new Blog({
+        title: 'Hello World 2',
+        body: 'hello everyone! this is my first thing in mongoDB'
+    })
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
+
+app.get('/blog/view', (req, res) => {
+    Blog.find()
+        .then((results) => {
+            res.send(results)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('/blog/create', (req, res) => {
+    res.render('create-blog')
+})
+
+app.post('/blog/create', (req, res) => {
+    const blog = new Blog({
+        title: String(req.title),
+        body: String(req.body)
+    })
+    blog.save()
+        .catch((err) => {
+            console.log(err)
+        })
 })
